58f155687094c9e366c3d2db300f5a82fd566435
Sam Jewell <sam.jewell@cantab.net>
2016-10-20 16:22:40 +0100

    Add a couple more aliases

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -9,5 +9,8 @@ alias reset_test_db="RAILS_ENV=test rake db:drop db:create db:structure:load db:
 alias branchdb="branchy --enable; rake db:create db:migrate"
 alias tail_action="tail -f log/development.log | grep 'Processing by'"
 
+alias retro="subl /Users/samjewell/Library/Mobile\ Documents/com~apple~CloudDocs/markdown-notes"
+alias start_review="open https://docs.google.com/spreadsheets/d/1Os-8dbuUqR7-YRAz1BBPnC34pqhZUWZEYeJhmro4hsU/edit"
+
 # Other
 # alias browsync='browser-sync start --proxy localhost:3000 --files "app/assets/stylesheets/**/*.scss, app/views/**/*.html.haml"'
