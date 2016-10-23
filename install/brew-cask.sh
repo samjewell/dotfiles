e54b10ddc109ea20a0f9dece6a6a3e0056f25116
Sam Jewell <sam.jewell@cantab.net>
2016-10-23 10:21:34 +0100

    Remove manual installs from `brew-cask.sh`

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -17,19 +17,8 @@ apps=(
   sourcetree
   spectacle
   valentina-studio
+  soundflower
+  soundcast
 )
 
 brew cask install "${apps[@]}"
-
-### Then install these manually: ###
-# node
-# docker for mac
-# Firefox 46
-# postgresapp
-# sublime-text3
-# oh-my-zsh
-
-### And for capybara, you'll need these:
-###   (see https://github.com/thoughtbot/capybara-webkit/wiki/Installing-Qt-and-compiling-capybara-webkit)
-# xcode
-# qt5
