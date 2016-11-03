60df5af9c25a95142aa3fa0b08d48228553590ad
Sam Jewell <sam.jewell@cantab.net>
2016-11-03 14:23:41 +0000

    Update the OSX defaults, having tried them out

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -1,49 +1,36 @@
 #!/bin/bash
 ############################
-# Inspired by https://github.com/webpro/dotfiles
+# Inspired by https://github.com/webpro/dotfiles and
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 
 # There are a HUGE number of these online - take your pick, and add them here!
 
-COMPUTER_NAME="BU Developer Mac"
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until this script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
 
-# Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "$COMPUTER_NAME"
-sudo scutil --set HostName "$COMPUTER_NAME"
-sudo scutil --set LocalHostName "$COMPUTER_NAME"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
-
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
-
-# Menu bar: show battery percentage
+# Menu bar: show battery percentage - nope
 defaults write com.apple.menuextra.battery -bool true
 
-# Increase window resize speed for Cocoa applications
+# Increase window resize speed for Cocoa applications - nope
 defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 
 # Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
 # Expand print panel by default
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-# Save to disk (not to iCloud) by default
+# Save to disk (not to iCloud) by default - nope
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-# Automatically quit printer app once the print jobs complete
+# Automatically quit printer app once the print jobs complete - nope ??
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# Disable the “Are you sure you want to open this application?” dialog
+# Disable the “Are you sure you want to open this application?” dialog - nope
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 
@@ -51,13 +38,27 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
 ###############################################################################
 
-# Trackpad: enable tap to click for this user and for the login screen
+# Trackpad: enable tap to click for this user and for the login screen - nope
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+# Enable full keyboard access for all controls - nope ??
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+# Follow the keyboard focus while zoomed in
+defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate - WORKS
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Automatically illuminate built-in MacBook keyboard in low light
 defaults write com.apple.BezelServices kDim -bool true
@@ -69,7 +70,7 @@ defaults write com.apple.BezelServices kDimTime -int 300
 # Screen                                                                      #
 ###############################################################################
 
-# Require password 5 seconds after sleep or screen saver begins
+# Require password 5 seconds after sleep or screen saver begins - nope ?? (reboot)
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 5
 
@@ -93,6 +94,9 @@ defaults write com.apple.finder ShowStatusBar -bool true
 # Finder: show path bar
 defaults write com.apple.finder ShowPathbar -bool true
 
+# When performing a search, search the current folder by default - nope ??
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
@@ -109,6 +113,9 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
+# Dock to show and hide very fast
+defaults write com.apple.Dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.5
 
 ###############################################################################
 # Hot corners                                                                 #
@@ -130,7 +137,7 @@ defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock wvous-tl-corner -int 0
 defaults write com.apple.dock wvous-tl-modifier -int 0
 
-# Top right screen corner → Display to sleep
+# Top right screen corner → Display to sleep - nope ??
 defaults write com.apple.dock wvous-tr-corner -int 10
 defaults write com.apple.dock wvous-tr-modifier -int 0
 
@@ -144,10 +151,18 @@ defaults write com.apple.dock wvous-br-modifier -int 0
 
 
 ###############################################################################
+# Photos                                                                      #
+###############################################################################
+
+# Prevent Photos from opening automatically when devices are plugged in - nope ??
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+
+###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Dock" "Finder" "SystemUIServer"; do
+for app in "Dock" "Finder" "SystemUIServer" "Photos"; do
   killall "${app}" &> /dev/null
 done
 
