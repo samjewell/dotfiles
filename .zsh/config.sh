bbdac301e1b4f571722165bfd4ea429d89042f54
Sam Jewell <sam.jewell@cantab.net>
2016-11-28 11:54:42 +0000

    Change npm's default directory, to install ESLint globally

--- .zsh/config.sh
+++ .zsh/config.sh
@@ -2,6 +2,9 @@ export EDITOR='subl -w'
 # Add psql to PATH
 export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/9.6/bin"
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+# Change npm's default directory for global modules
+# see https://docs.npmjs.com/getting-started/fixing-npm-permissions#option-2-change-npms-default-directory-to-another-directory
+export PATH=~/.npm-global/bin:$PATH
 
 # first `brew install direnv`, then
 eval "$(direnv hook zsh)"
