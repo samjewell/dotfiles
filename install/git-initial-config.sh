a8372b0414ca7ff6ff064b9ffdc9a5990795bfa8
Sam Jewell <sam.jewell@cantab.net>
2017-12-21 13:41:02 +0000

    Always prune local branches when fetching origin

--- install/git-initial-config.sh
+++ install/git-initial-config.sh
@@ -2,3 +2,4 @@ git config --global rerere.enabled true
 git config --global diff.compactionHeuristic true
 git config --global push.default current
 git config --global core.mergeoptions --no-edit
+git config --global remote.origin.prune true
