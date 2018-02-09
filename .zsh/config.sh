0417f6c6b498bcadf862e1bffcd9feb7ee9818dd
Sam Jewell <sam.jewell@cantab.net>
2018-02-09 08:55:59 +0000

    Suppress deprecation warnings in BridgeU repo

--- .zsh/config.sh
+++ .zsh/config.sh
@@ -5,6 +5,7 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 # Change npm's default directory for global modules
 # see https://docs.npmjs.com/getting-started/fixing-npm-permissions#option-2-change-npms-default-directory-to-another-directory
 export PATH=~/.npm-global/bin:$PATH
+export SUPPRESS_DEPRECATION_WARNINGS='true'
 
 # first `brew install direnv`, then
 eval "$(direnv hook zsh)"
