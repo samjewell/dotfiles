983852e45a7c35e1611e81251948a15765f5caa9
Sam Jewell <sam.jewell@cantab.net>
2018-07-09 11:23:05 +0100

    Extend `tail_action` to print request params

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -14,7 +14,7 @@ alias mv="echo won’t clobber && mv -n"
 # BU stuff here
 alias reset_test_db="RAILS_ENV=test rake db:drop db:create db:schema:load db:structure:load"
 alias branchdb="branchy --enable; rake db:create db:migrate"
-alias tail_action="tail -f log/development.log | grep 'Processing by'"
+alias tail_action="tail -f log/development.log | grep -E 'Processing by|Parameters:'"
 
 alias retro="subl /Users/samjewell/Library/Mobile\ Documents/com~apple~CloudDocs/markdown-notes"
 alias start_review="open https://docs.google.com/spreadsheets/d/1Os-8dbuUqR7-YRAz1BBPnC34pqhZUWZEYeJhmro4hsU/edit"
