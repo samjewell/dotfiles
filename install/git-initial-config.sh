8fd09a3e97ad6b9f2bec3579077b8c0575c8f103
Sam Jewell <sam.jewell@cantab.net>
2019-03-04 08:59:25 +0000

    Run Rubocop as a commit hook

--- install/git-initial-config.sh
+++ install/git-initial-config.sh
@@ -4,3 +4,4 @@ git config --global push.default current
 git config --global core.mergeoptions --no-edit
 git config --global remote.origin.prune true
 git config --global rebase.autoStash true
+git config --global core.hooksPath ~/dotfiles/commit_hooks
