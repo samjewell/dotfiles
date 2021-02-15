0bba770ad8d7c001cbd984b0ed793f53640684c4
Sam Jewell <sam.jewell@cantab.net>
2021-02-15 17:00:18 +0000

    Only install the minimum apps that I need

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -6,21 +6,21 @@ brew tap caskroom/cask
 # Install packages
 
 apps=(
-  anaconda
+  # anaconda
   dropbox
   google-chrome
   iterm2
   # karabiner
-  pycharm-ce
+  # pycharm-ce
   # sketch
   # sketch-toolbox
   slack
   sourcetree
-  spectacle
-  spotify
-  valentina-studio
-  soundflower
-  soundcast
+  # spectacle
+  # spotify
+  # valentina-studio
+  # soundflower
+  # soundcast
 )
 
 brew cask install "${apps[@]}"
