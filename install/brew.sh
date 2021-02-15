f8c820214ba32c783f7a9ab5f1783b0fdc09ecb9
Sam Jewell <sam.jewell@cantab.net>
2021-02-15 16:59:50 +0000

    Straighten out the presentation of these packages

--- install/brew.sh
+++ install/brew.sh
@@ -3,16 +3,18 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install packages
-# mas tool installs apps from the App Store, via the command-line
-brew install git rmtrash heroku # mas
-# OSX python doesn't include pip. brew python does include it. But I've actually
-# found I like using Conda instead, for now.
-#brew install python
-# Postpone installing yarn until I need it
-#brew install yarn
-# Consider getting direnv or autoenv, to automatically load env-vars and python
-# environments when entering/exiting folders
-# Consider running Redis and Postgres via Docker only
+packages=(
+  git
+  rmtrash
+  heroku
+  # mas # installs apps from the App Store, via the command-line
+  # python # MacOS python doesn't include pip, but this does. But postpone, and use Conda instead?
+  # yarn
+  # direnv # also autoenv, to automatically load env-vars and python envs when entering/exiting folders
+  # redis # Run through Docker instead?
+  # postgresql # Run through Docker instead?
+)
+brew install "${packages[@]}"
 
 # Install Oh My Zsh, via curl
 # Big Sur includes ZSH by default now
