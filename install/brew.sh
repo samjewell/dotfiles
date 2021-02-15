3acf55badd8dd8fa0947469f958e2f535cbb57d7
Sam Jewell <sam.jewell@cantab.net>
2021-02-15 16:45:39 +0000

    postpone direnv/autoenv
    These can be set up later.

--- install/brew.sh
+++ install/brew.sh
@@ -5,7 +5,9 @@
 # Install packages
 # mas tool installs apps from the App Store, via the command-line
 # OSX python doesn't include pip. brew python does include it
-brew install git direnv redis mas python yarn postgresql rmtrash heroku circleci watch
+brew install git redis mas python yarn postgresql rmtrash heroku circleci watch
+# Consider getting direnv or autoenv, to automatically load env-vars and python
+# environments when entering/exiting folders
 
 # Install Oh My Zsh, via curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
