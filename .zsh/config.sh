8574095d86f8c04f0859d3184d1d54c73328972c
Sam Jewell <sam.jewell@cantab.net>
2021-02-15 16:30:49 +0000

    Add node 14 to path

--- .zsh/config.sh
+++ .zsh/config.sh
@@ -3,6 +3,8 @@ export EDITOR='subl -w'
 export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin"
 # Add anaconda to PATH
 export PATH=/usr/local/anaconda3/bin:"$PATH"
+# Add node 14 to PATH
+export PATH="/usr/local/opt/node@14/bin:$PATH"
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 # Change npm's default directory for global modules
 # see https://docs.npmjs.com/getting-started/fixing-npm-permissions#option-2-change-npms-default-directory-to-another-directory
