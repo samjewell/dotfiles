b92655b90297c8b4eac7e42f6cdb1e1d34add224
Sam Jewell <sam.jewell@cantab.net>
2021-02-15 16:47:24 +0000

    Consider running Postgres and Redis through docker

--- install/brew.sh
+++ install/brew.sh
@@ -5,9 +5,10 @@
 # Install packages
 # mas tool installs apps from the App Store, via the command-line
 # OSX python doesn't include pip. brew python does include it
-brew install git redis mas python yarn postgresql rmtrash heroku circleci watch
+brew install git mas python yarn rmtrash heroku circleci watch
 # Consider getting direnv or autoenv, to automatically load env-vars and python
 # environments when entering/exiting folders
+# Consider running Redis and Postgres via Docker only
 
 # Install Oh My Zsh, via curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
