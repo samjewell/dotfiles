fe3853da00216c5f790761b6946713314240430f
Sam Jewell <2903904+samjewell@users.noreply.github.com>
2021-02-16 16:22:55 +0000

    Update aliases, according to new `trash` brew pkg

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -6,12 +6,11 @@ alias hidehidden='defaults write com.apple.finder AppleShowAllFiles NO'
 alias zshconfig="subl ~/dotfiles"
 
 # Stay safe out there
-alias trash="rmtrash"
 alias rm="echo Use 'trash', or the full path i.e. '/bin/rm'"
 alias cp="echo won’t clobber && cp -n"
 alias mv="echo won’t clobber && mv -n"
 
-# BU stuff here
+# Rails stuff here
 alias reset_test_db="RAILS_ENV=test rails db:drop db:create db:schema:load db:structure:load"
 alias branchdb="branchy --enable; rake db:create db:migrate"
 alias tail_action="tail -f log/development.log | grep -E 'Processing by|Parameters:'"
@@ -20,5 +19,4 @@ alias retro="subl /Users/samjewell/Library/Mobile\ Documents/com~apple~CloudDocs
 alias start_review="open https://docs.google.com/spreadsheets/d/1Os-8dbuUqR7-YRAz1BBPnC34pqhZUWZEYeJhmro4hsU/edit"
 
 # Other
-# alias browsync='browser-sync start --proxy localhost:3000 --files "app/assets/stylesheets/**/*.scss, app/views/**/*.html.haml"'
 alias list_flippers='grep -hoR "\$flipper\[:\(\w\+\)]" --exclude-dir=tmp --exclude-dir=log --exclude-dir=node_modules --exclude-dir=coverage --color=none * | sort -u | cut -d "[" -f2 | cut -d "]" -f1'
