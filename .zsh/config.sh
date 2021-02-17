faa6dde796ddcbbe95d32e9be0e5ec43f9c6f1bb
Sam Jewell <2903904+samjewell@users.noreply.github.com>
2021-02-17 16:18:37 +0000

    Only enable Go config in config.sh
    This means I can include `config.sh` in my .zshrc file 🎉

--- .zsh/config.sh
+++ .zsh/config.sh
@@ -1,22 +1,22 @@
-# Add psql to PATH
-export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin"
-# Add anaconda to PATH
-export PATH=/usr/local/anaconda3/bin:"$PATH"
-# Add node 14 to PATH
-export PATH="/usr/local/opt/node@14/bin:$PATH"
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-# Change npm's default directory for global modules
-# see https://docs.npmjs.com/getting-started/fixing-npm-permissions#option-2-change-npms-default-directory-to-another-directory
-export PATH=~/.npm-global/bin:$PATH
-# Add vault to PATH
-export PATH=$PATH:/Applications/vault
-export SUPPRESS_DEPRECATION_WARNINGS='true'
-export BULLET_ENABLED='true'
-export CAPYBARA_DRIVER='chrome_headless'
-export DEBUG='true'
+# # Add psql to PATH
+# export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin"
+# # Add anaconda to PATH
+# export PATH=/usr/local/anaconda3/bin:"$PATH"
+# # Add node 14 to PATH
+# export PATH="/usr/local/opt/node@14/bin:$PATH"
+# export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+# # Change npm's default directory for global modules
+# # see https://docs.npmjs.com/getting-started/fixing-npm-permissions#option-2-change-npms-default-directory-to-another-directory
+# export PATH=~/.npm-global/bin:$PATH
+# # Add vault to PATH
+# export PATH=$PATH:/Applications/vault
+# export SUPPRESS_DEPRECATION_WARNINGS='true'
+# export BULLET_ENABLED='true'
+# export CAPYBARA_DRIVER='chrome_headless'
+# export DEBUG='true'
 
-# first `brew install direnv`, then
-eval "$(direnv hook zsh)"
+# # first `brew install direnv`, then
+# eval "$(direnv hook zsh)"
 
 # Add Go to PATH
 export GOPATH=$HOME/go
