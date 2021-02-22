fa32bce8d79cce4f84fdd127dcfbedf1a662a3f5
Sam Jewell <2903904+samjewell@users.noreply.github.com>
2021-02-22 08:37:30 +0000

    Update my welcome message to be kinder on my soul

--- .zsh/init.sh
+++ .zsh/init.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # ssh-add -K ~/.ssh/bu_gh_id_rsa
-echo "Keep smiling, and fake it til you make it 😉"
+
+echo " __                 "
+echo "/ _\ __ _ _ __ ___  "
+echo "\ \ / _\` | '_ \` _ \ "
+echo "_\ \ (_| | | | | | |"
+echo "\__/\__,_|_| |_| |_|"
+                    
+# echo "Keep smiling, and fake it til you make it 😉"
+echo "Take confidence and enjoy your work"
+echo "- We’re all figuring it out as we go! 🤓😉😌"
 
 # # Allow [ or ] whereever you want
 # unsetopt nomatch
