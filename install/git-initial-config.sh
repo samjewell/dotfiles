48bcd1c5f6209f625609693ae4d8fbc519963667
Sam Jewell <sam.jewell@grafana.com>
2023-11-15 09:21:26 +0000

    Never manually track a branch on the remote again:
    See this answer
    https://stackoverflow.com/questions/32056324/there-is-no-tracking-information-for-the-current-branch#comment132548135_35487209

--- install/git-initial-config.sh
+++ install/git-initial-config.sh
@@ -1,4 +1,5 @@
 # git config --global core.hooksPath ~/dotfiles/commit_hooks
+git config --global branch.autoSetupMerge simple
 git config --global commit.verbose true
 git config --global core.autocrlf false # https://github.com/hashicorp/terraform-provider-aws/issues/5011#issuecomment-717107831
 git config --global core.excludesfile "/Users/samjewell/dotfiles/.gitignore_global"
@@ -7,6 +8,7 @@ git config --global diff.compactionHeuristic true
 git config --global diff.renameLimit 5500
 git config --global init.defaultBranch main
 git config --global pull.ff only
+git config --global push.autoSetupRemote true
 git config --global push.default current
 git config --global rebase.autoStash true
 git config --global remote.origin.prune true
