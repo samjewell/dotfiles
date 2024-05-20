d3a61d0f98ffdc1a4a5aa050532260dccc866d97
Sam Jewell <sam.jewell@grafana.com>
2024-05-20 17:11:21 +0100

    Update my welcome message. Remove ref to old SSH key

--- .zsh/init.sh
+++ .zsh/init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# ssh-add -K ~/.ssh/bu_gh_id_rsa
-
 echo " __                 "
 echo "/ _\ __ _ _ __ ___  "
 echo "\ \ / _\` | '_ \` _ \ "
@@ -11,6 +9,10 @@ echo "\__/\__,_|_| |_| |_|"
 # echo "Keep smiling, and fake it til you make it 😉"
 echo "Be ambitous - and build new capabilities"
 echo "- We’re all figuring it out as we go! 🤓😉😌"
+echo ""
+echo "Also, ENJOY YOUR WORK"
+echo "And 'Don't give too many fucks' - Mark Manson"
+
 
 # # Allow [ or ] whereever you want
 # unsetopt nomatch
